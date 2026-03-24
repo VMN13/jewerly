f630f6fd262ded6d2e66628ff77e7e12ec380ba2
VMN13 <Stayspaceflux@mail.com>
2026-03-24 23:15:18 +0300

    no coockies

--- src/components/CookieConsent.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { useEffect, useState, useCallback } from 'react';
-import styles from './CookieConsent.module.css';
-
-export default function CookieConsent() {
-  const [isVisible
